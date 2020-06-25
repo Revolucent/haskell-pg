0a84952e4e05c6809c677baa2c8dc6040116e658
Gregory Higley <greg@prosumma.com>
2020-06-25 13:04:04 -0400

    PG

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell #-}
-
 module Main where
-
-import Control.Monad.IO.Class
-import Data.Aeson
-import Data.Aeson.TH
-import Database.PostgreSQL.PG
-import Database.PostgreSQL.Simple.FromRow (FromRow(..), field)
 
 main = putStrLn "ok"
--- src/Database/PostgreSQL/PG.hs
+++ src/Database/PostgreSQL/PG.hs
@@ -1,84 +1,66 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Database.PostgreSQL.PG (
-    PG,
-    bracketWithConnection,
-    bracketWithConnectionString,
     execute,
     execute_,
-    getConnection,
+    liftPG,
     query,
     query_,
     query1_,
     runPG,
-    withConnection,
-    withConnectionString,
     withTransaction
 ) where
 
 import Prelude hiding (head)
 
-import Control.Applicative (Alternative, Applicative)
 import Data.List.Safe (head)
-import Control.Monad (Monad)
-import Control.Monad.Catch (bracket, MonadCatch, MonadThrow)
+import Control.Monad.Catch (MonadCatch, MonadThrow)
 import Control.Monad.IO.Class (liftIO, MonadIO)
-import Control.Monad.Reader (ask, ReaderT(..))
+import Control.Monad.Reader (ask, MonadReader, ReaderT(..))
 import Data.ByteString (ByteString)
-import Database.PostgreSQL.Simple (Connection, Query, FromRow, ToRow, connectPostgreSQL, close, fromOnly, Only(..))
+import Database.PostgreSQL.Simple (Connection, Query, FromRow, ToRow, connectPostgreSQL)
 import qualified Database.PostgreSQL.Simple as Postgres
 import GHC.Int (Int64)
+
+type PG = ReaderT Connection IO
+
+runPG :: MonadIO m => PG a -> Connection -> m a
+runPG a = liftIO . runReaderT a
+
+liftPG :: (MonadIO m, MonadReader Connection m) => PG a -> m a
+liftPG a = ask >>= runPG a
+
+withTransaction :: (MonadIO m, MonadReader Connection m) => PG a -> m a
+withTransaction a = do 
+    conn <- ask
+    liftIO $ Postgres.withTransaction conn $ runPG a conn
 
 flip3 :: (a -> b -> c -> d) -> b -> c -> a -> d
 flip3 f b c a = f a b c
 
-newtype PG a = PG { unPG :: ReaderT Connection IO a } deriving (Functor, Applicative, Monad, MonadThrow, MonadCatch, MonadIO, MonadFail, Alternative)
+withConn1 :: (MonadReader Connection m, MonadIO m) => (Connection -> a -> IO b) -> a -> m b 
+withConn1 f a = ask >>= liftIO . flip f a
 
-getConnection :: PG Connection
-getConnection = PG ask
+withConn2 :: (MonadReader Connection m, MonadIO m) => (Connection -> a -> b -> IO c) -> a -> b -> m c
+withConn2 f a b = ask >>= liftIO . flip3 f a b 
 
-runPG :: MonadIO m => PG a -> Connection -> m a
-runPG (PG a) = liftIO . runReaderT a
-
-withConnection :: MonadIO m => Connection -> PG a -> m a
-withConnection conn a = runPG a conn
-
-withConnectionString :: MonadIO m => ByteString -> PG a -> m a 
-withConnectionString connectionString (PG a) = liftIO $ connectPostgreSQL connectionString >>= runReaderT a 
-
-bracketWithConnection :: Connection -> (Connection -> IO a) -> IO a
-bracketWithConnection conn = bracket (return conn) close 
-
-bracketWithConnectionString :: ByteString -> (Connection -> IO a) -> IO a
-bracketWithConnectionString connectionString = bracket (connectPostgreSQL connectionString) close 
-
-withConn1 :: (Connection -> a -> IO b) -> a -> PG b 
-withConn1 f a = getConnection >>= liftIO . flip f a
-
-withConn2 :: (Connection -> a -> b -> IO c) -> a -> b -> PG c
-withConn2 f a b = getConnection >>= liftIO . flip3 f a b 
-
-execute_ :: Query -> PG Int64 
+execute_ :: (MonadReader Connection m, MonadIO m) => Query -> m Int64 
 execute_ = withConn1 Postgres.execute_
 
-execute :: ToRow q => Query -> q -> PG Int64 
+execute :: (MonadReader Connection m, MonadIO m) => ToRow q => Query -> q -> m Int64 
 execute = withConn2 Postgres.execute 
 
-query_ :: FromRow r => Query -> PG [r]
+query_ :: (MonadReader Connection m, MonadIO m) => FromRow r => Query -> m [r]
 query_ = withConn1 Postgres.query_ 
 
-query :: (ToRow q, FromRow r) => Query -> q -> PG [r]
-query = withConn2 Postgres.query 
-
-query1_ :: FromRow r => Query -> PG r 
+query1_ :: (MonadReader Connection m, MonadIO m, MonadThrow m) => FromRow r => Query -> m r 
 query1_ sql = query_ sql >>= head
 
-query1 :: (ToRow q, FromRow r) => Query -> q -> PG r 
-query1 sql q = query sql q >>= head 
+query :: (MonadReader Connection m, MonadIO m) => (ToRow q, FromRow r) => Query -> q -> m [r]
+query = withConn2 Postgres.query 
 
-withTransaction :: PG a -> PG a
-withTransaction (PG a) = do
-    conn <- getConnection 
-    liftIO $ Postgres.withTransaction conn $ runReaderT a conn
+query1 :: (MonadReader Connection m, MonadIO m, MonadThrow m) => (ToRow q, FromRow r) => Query -> q -> m r 
+query1 sql q = query sql q >>= head
